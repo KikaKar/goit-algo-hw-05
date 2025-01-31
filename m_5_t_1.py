9651c61d43c7379b5263e8244bdb533cb3887b68
Kika_Kar <kirilokarnaruk@gmail.com>
2025-01-31 15:32:41 +0200

    Task 1 v 1.2

--- m_5_t_1.py
+++ m_5_t_1.py
@@ -1,21 +1,20 @@
 
 def caching_fibonacci():
-    cache = {}
+    cache = {} # порожній словник cache
 
     def fibonacci(n):
         if n <= 0:
             return 0
         if n == 1:
             return 1
-        if n in cache:
+        if n in cache: # Якщо значення є у кеші воно зберігається та повертається 
             return cache[n]
         
-        cache[n] = fibonacci(n -1) + fibonacci(n - 2)
+        cache[n] = fibonacci(n -1) + fibonacci(n - 2) # Обчислення значення числа Фібоначчі
         return cache[n]
     
     return fibonacci
 
 fib = caching_fibonacci()
 print(fib(10))
-print(fib(20))
-print(fib(35))
+print(fib(15))
