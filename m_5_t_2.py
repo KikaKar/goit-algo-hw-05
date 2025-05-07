bbd10391ce44a539ff754117230fedc49c31a629
Kika_Kar <kirilokarnaruk@gmail.com>
2025-05-07 16:44:51 +0300

    Task 2 v 1.2

--- m_5_t_2.py
+++ m_5_t_2.py
@@ -1,7 +1,17 @@
-text = "1 Абаламага 2"
+import re
+from typing import Callable, Generator
 
-def generator_numbers(text: str):
-    digits = "".join(filter(str.isdigit, text))
-    return digits
-    #def sum_profit(text: str, func: Callable):
-print (generator_numbers(text))
+text = ("Загальний дохід працівника складається з декількох частин: "
+"1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів.")
+
+def generator_numbers(text: str) -> Generator[float, None, None]:
+    # Виокремлються числа з тексту
+    for match in re.findall(r'(?<=\s)\d+\.\d+(?=\s)|^\d+\.\d+(?=\s)|(?<=\s)\d+\.\d+$', text):
+        yield float(match)
+
+def sum_profit(text: str, func: Callable[[str], Generator[float, None, None]]) -> float:
+    # Підрахунок суми чисел
+    return sum(func(text))
+
+total_income = sum_profit(text, generator_numbers)
+print(f"Загальний дохід: {total_income}")
