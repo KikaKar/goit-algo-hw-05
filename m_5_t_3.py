fad0041132611ab66085d063ccdf58f9b5387e5f
Kika_Kar <kirilokarnaruk@gmail.com>
2025-07-11 17:21:51 +0300

    Task 3 v 0.2.1

--- m_5_t_3.py
+++ m_5_t_3.py
@@ -1,10 +1,17 @@
 from pathlib import Path
+import sys
+
+
+# Відкриття файлу
 
 file_path = input('Введіть шлях до файлу: ')
 path = Path(file_path)
 
-if path.exists() and path.is_file():
+if path.exists() and path.is_file(): # перевірка існування файлу за вказаним посиланням
     text = path.read_text()
-    print(text)
+    print('Прийнято файл логів')
 else:
     print("Файл не знайдено або шлях некоректний.")
+
+# Необов'язковий аргумент командного рядка, виведення всі записи певного рівння логування
+
